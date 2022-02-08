4b31c72c170a8e98c58d3d1f99e85862aa82a4b0
ProCodr <malireza@ymail.com>
2022-02-08 22:49:51 +0330

    make CCI indicator generic

--- indicator_cci.go
+++ indicator_cci.go
@@ -3,23 +3,22 @@ package techan
 import "github.com/sdcoffey/big"
 
 type commidityChannelIndexIndicator struct {
-	series *TimeSeries
-	window int
+	ccIndicator Indicator
+	window      int
 }
 
 // NewCCIIndicator Returns a new Commodity Channel Index Indicator
 // http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:commodity_channel_index_cci
-func NewCCIIndicator(ts *TimeSeries, window int) Indicator {
+func NewCCIIndicator(indicator Indicator, window int) Indicator {
 	return commidityChannelIndexIndicator{
-		series: ts,
-		window: window,
+		ccIndicator: indicator,
+		window:      window,
 	}
 }
 
 func (ccii commidityChannelIndexIndicator) Calculate(index int) big.Decimal {
-	typicalPrice := NewTypicalPriceIndicator(ccii.series)
-	typicalPriceSma := NewSimpleMovingAverage(typicalPrice, ccii.window)
-	meanDeviation := NewMeanDeviationIndicator(NewClosePriceIndicator(ccii.series), ccii.window)
+	typicalPriceSma := NewSimpleMovingAverage(ccii.ccIndicator, ccii.window)
+	meanDeviation := NewMeanDeviationIndicator(ccii.ccIndicator, ccii.window)
 
-	return typicalPrice.Calculate(index).Sub(typicalPriceSma.Calculate(index)).Div(meanDeviation.Calculate(index).Mul(big.NewFromString("0.015")))
+	return ccii.ccIndicator.Calculate(index).Sub(typicalPriceSma.Calculate(index)).Div(meanDeviation.Calculate(index).Mul(big.NewFromString("0.015")))
 }
